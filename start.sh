38b64e65c64b41e5969ef5a20c06e70a9a316e78
Joao Bosco Jares MSc <jbjares@gmail.com>
2019-11-10 20:52:16 +0100

    minor fix

--- start.sh
+++ start.sh
@@ -3,59 +3,51 @@
 echo "start script running..."
 
 if ([ $1 = "--init" ] && [ $2 = true ] && [ $3 = "--env" ] && [ $4 = "dev" ]); then
-
-    cd /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red
     sudo npm install
     sudo npm run build
-    #npm start
-    sudo cd /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/packages/node_modules/node-red
-    sudo node red.js -s /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/client-installation/settings-localhost.js gofairflows_.local.json
+    sudo cd packages/node_modules/node-red
+    sudo node red.js -s  ./client-installation/settings-localhost.js ./client-installation/gofairflows_.local.json
 
 fi
 
 if ([ $1 = "--refresh" ] && [ $2 = true ] && [ $3 = "--env" ] && [ $4 = "dev" ]); then
-    cd /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/packages/node_modules/node-red
-    sudo node red.js -s /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/client-installation/settings-localhost.js gofairflows_.local.json
+    cd packages/node_modules/node-red
+    sudo node red.js -s ./client-installation/settings-localhost.js  ./client-installation/gofairflows_.local.json
 
 fi
 
 if ([ $1 = "--init" ] && [ $2 = true ] && [ $3 = "--env" ] && [ $4 = "dosrv02" ]); then
 
     echo "--init --env: dosrv02 loading..."
-    cd /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red
     sudo npm install
     sudo npm run build
-    #npm start
-    cd /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/packages/node_modules/node-red
-    sudo node red.js -s /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/client-installation/settings-all-digitalocean.js gofairflows_.dosrv02.json
+    cd packages/node_modules/node-red
+    sudo node red.js -s ./client-installation/settings-all-digitalocean.js ./node-red/client-installation/gofairflows_.dosrv02.json
     echo "--init --env: dosrv02 loaded!"
 fi
 
 if ([ $1 = "--refresh" ] && [ $2 = true ] && [ $3 = "--env" ] && [ $4 = "dosrv02" ]); then
     echo "--refresh --env: dosrv02 loading..."
-    cd /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/packages/node_modules/node-red
-    sudo node red.js -s /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/client-installation/settings-all-digitalocean.js gofairflows_.dosrv02.json
+    cd packages/node_modules/node-red
+    sudo node red.js -s ./client-installation/settings-all-digitalocean.js ./node-red/client-installation/gofairflows_.dosrv02.json
     echo "--refresh --env: dosrv02 loaded!"
 fi
 
 
 
 if ([ $1 = "--init" ] && [ $2 = true ] && [ $3 = "--env" ] && [ $4 = "cloud41" ]); then
-
     echo "--init --env: cloud41 loading..."
-    cd /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red
     sudo npm install
     sudo npm run build
-    #npm start
-    cd /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/packages/node_modules/node-red
-    sudo node red.js -s /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/client-installation/settings-cloud41.js /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/client-installation/gofairflows_.cloud41.json
+    cd packages/node_modules/node-red
+    sudo node red.js -s ./client-installation/settings-cloud41.js ./client-installation/gofairflows_.cloud41.json
     echo "--init --env: cloud41 loaded!"
 fi
 
 if ([ $1 = "--refresh" ] && [ $2 = true ] && [ $3 = "--env" ] && [ $4 = "cloud41" ]); then
     echo "--refresh --env: cloud41 loading..."
-    cd /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/packages/node_modules/node-red
-    sudo node red.js -s /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/client-installation/settings-cloud41.js /Users/jbjares/workspaces/PHTWorkspace/central_workspace/train4fair/node-red/client-installation/gofairflows_.cloud41.json
+    cd packages/node_modules/node-red
+    sudo node red.js -s ./client-installation/settings-cloud41.js ./client-installation/gofairflows_.cloud41.json
     echo "--refresh --env: cloud41 loaded!"
 fi
 
